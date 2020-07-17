419659baf1f6975d735c8d958aa84165852c5cc6
James Armah <armahjames9@gmail.com>
2020-07-17 11:18:51 +0000

    Version 0.4 Stuck on second section, Look into it later

--- src/components/SecondPart.js
+++ src/components/SecondPart.js
@@ -1,9 +1,58 @@
-import React from 'react';
-import { GrInProgress } from 'react-icons/gr';
+import React, { useState } from "react";
+import { GrInProgress } from "react-icons/gr";
+import "react-bootstrap-range-slider/dist/react-bootstrap-range-slider.css";
+import RangeSlider from "react-bootstrap-range-slider";
+import "../bootstrap.min.css";
+import "../General.css";
+
+const MyComponent = () => {
+  const [value, setValue] = useState(1000)
+
+  return (
+    <RangeSlider
+      value={value}
+      onChange={changeEvent => setValue(changeEvent.target.value)}
+      size='lg'
+    />
+  )
+}
 
 const SecondPart = () => (
-    <div>
-        <GrInProgress /> Second Division 
+  <div>
+    <div className="container">
+        <GrInProgress />
+        <br />
+        <br />
+        <div className="row">
+            <div className="col-sm-1">
+
+            </div>
+            <div className="col-sm-10">
+                <div className="cardedits card">
+                    <div className="card-body">
+                    <br />
+                        <div className="titleedits">
+                            <span className="title"><h2> Choose your debt amount </h2></span>
+                            <div className="row">
+                              <div className="col-sm-3">
+
+                              </div>
+                              <div className="col-sm-6">
+                                <span className="trial"> <MyComponent /> </span>
+                              </div>
+                              <div className="col-sm-3">
+
+                              </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="col-sm-1">
+            </div>
+        </div>
+        <br />
     </div>
+  </div>
 )
 export default SecondPart
